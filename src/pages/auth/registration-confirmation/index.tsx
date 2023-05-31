60132254b5aa02ed0e7227c34b5f63a6a525e81a
grigorydobrenko <grigorydobrenko97@gmail.com>
2023-05-31 17:29:32 +0300

    feat: add sending confirmation token

---
+++ src/pages/auth/registration-confirmation/index.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react'
+
+import { useRouter } from 'next/router'
+
+import { useRegistrationConfirmationMutation } from 'features/registration-confirmation/service/registrationConfirmation'
+import Confirmation from 'features/registration-confirmation/ui/Confirmation'
+import cls from 'pages/auth/registration-confirmation/RegistrationConfirmation.module.scss'
+import { getLayout } from 'widgets/Layout/Layout'
+
+const RegistrationConfirmation = () => {
+  const [confirm] = useRegistrationConfirmationMutation()
+
+  const router = useRouter()
+
+  const token = router.query.code
+
+  useEffect(() => {
+    if (typeof token === 'string') {
+      confirm({ confirmationCode: token })
+    }
+  }, [])
+
+  return (
+    <div className={cls.registration_container}>
+      <Confirmation />
+    </div>
+  )
+}
+
+RegistrationConfirmation.getLayout = getLayout
+export default RegistrationConfirmation
