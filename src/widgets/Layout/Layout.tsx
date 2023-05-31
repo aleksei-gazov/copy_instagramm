1a59a6b07bbaa10ef0302f81937b896b427b5722
MAKS <5059585@mail.ru>
2023-05-31 17:22:42 +0300

    fix: Layout

--- src/widgets/Layout/Layout.tsx
+++ src/widgets/Layout/Layout.tsx
@@ -9,26 +9,10 @@ import { Header } from '../Header'
 import cls from './Layout.module.scss'
 
 import { AuthProvider } from 'shared/hoc'
-import { Portal } from 'shared/ui/Portal/Portal'
 import { store } from 'store/store'
 
 export const Layout: NextPage<PropsWithChildren> = props => {
   const { children } = props
-  // const [logOut] = useLogOutMutation()
-  //TODO
-  const email = 'MAIL' //modal
-  const [showModal, setShowModal] = useState<boolean>(false) //modal
-  const closeModal = () => {
-    setShowModal(false)
-  }
-  const onSubmit = () => {
-    //TODO
-    console.log('logOut')
-    // logOut()
-    setShowModal(false)
-    //TODO
-    //навигация на логин
-  }
 
   return (
     <AuthProvider>
