4cf03ed0bef554c85f30f258876605757971edf9
Sergey Saprankov <sergey-saprankov@mail.ru>
2023-05-31 15:42:40 +0300

    refactor: LogOutComponent.tsx test

--- src/features/LogOut/ui/LogOutComponent.tsx
+++ src/features/LogOut/ui/LogOutComponent.tsx
@@ -1,8 +1,14 @@
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 import logOutImg from '../../../../public/icon/logOut.svg'
 
+import { clearToken } from 'features/login'
+import { useLogOutMutation } from 'features/LogOut/service/logOut'
+import { PATH } from 'shared/const/path'
+import { useAppDispatch } from 'shared/hooks/useAppDispatch'
 import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button'
+import { Loader } from 'shared/ui/Loader/Loader'
 import cls from 'widgets/Header/ui/Header.module.scss'
 
 type PropsType = {
@@ -10,19 +16,22 @@ type PropsType = {
 }
 
 export const LogOutComponent = ({ setShowModal }: PropsType) => {
+  const router = useRouter()
+  const dispatch = useAppDispatch()
+  const [logOut, { isLoading }] = useLogOutMutation()
   const logOutHandler = () => {
-    setShowModal(true)
+    logOut()
+      .unwrap()
+      .then(() => {
+        dispatch(clearToken())
+        router.push(PATH.LOGIN)
+      })
   }
 
+  if (isLoading) return <Loader />
+
   return (
-    <Button
-      // disabled={true}
-      // type={'submit'}
-      className={cls.mb18}
-      theme={ButtonTheme.Clear}
-      size={ButtonSize.XXl}
-      onClick={logOutHandler}
-    >
+    <Button className={cls.mb18} theme={ButtonTheme.Clear} onClick={logOutHandler}>
       <Image src={logOutImg} alt={'icon github'} width={90} height={150} />
     </Button>
   )
