478f73e7d6bcb34d9b4cf42bd6a482725382ddab
Sergey Saprankov <sergey-saprankov@mail.ru>
2023-06-03 10:37:55 +0300

    feat: add checkbox

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,22 @@
 import React, { useState } from 'react'
 
-import { getLayout } from 'widgets/Layout/Layout'
-
 import { CheckBox } from 'shared/ui/Checkbox/Checkbox'
+import { getLayout } from 'widgets/Layout/Layout'
 
 function Home() {
   const [isChecked, setChecked] = useState(false)
 
   return (
-    <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}>
-        <CheckBox disabled={true} value={isChecked} onChangeChecked={setChecked} width={'18px'} height={'18px'} />
+    <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
+      <CheckBox
+        label={'Check-box'}
+        isChecked={isChecked}
+        onChangeChecked={setChecked}
+        width={'18px'}
+        height={'18px'}
+      />
     </div>
   )
-  
 }
 
 Home.getLayout = getLayout
--- src/shared/ui/Checkbox/Checkbox.tsx
+++ src/shared/ui/Checkbox/Checkbox.tsx
@@ -2,55 +2,58 @@ import React, { ChangeEvent, FC, memo, useState } from 'react'
 
 import Image from 'next/image'
 
-import cls from './Checkbox.module.scss'
-
 import check from '../../../../public/icon/checkbox-check-black.svg'
 import disabledCheckbox from '../../../../public/icon/checkbox-check-disabled.svg'
+
+import cls from './Checkbox.module.scss'
+
 import { classNames } from 'shared/lib/classNames/classNames'
-import { log } from 'console'
 
 interface CheckBoxProps {
   className?: string
   label?: string
   width: string
   height: string
-  value: boolean
+  isChecked: boolean
   onChangeChecked: (value: boolean) => void
-  disabled: boolean
+  disabled?: boolean
 }
 
 export const CheckBox: FC<CheckBoxProps> = memo(
-  ({ className = '', label, width, height, value, onChangeChecked, disabled }) => {
-   
+  ({ className = '', label, width, height, isChecked, onChangeChecked, disabled = false }) => {
     const onClickHandler = () => {
-      if(!disabled) {
-        onChangeChecked(!value)
+      if (!disabled) {
+        onChangeChecked(!isChecked)
       }
-      
     }
     const onChange = (e: ChangeEvent<HTMLInputElement>) => {
       onChangeChecked(e.currentTarget.checked)
     }
 
-    console.log(value)
+    const mods = {
+      [cls.isDisabled]: disabled,
+      [cls.isChecked]: isChecked,
+      [cls.disableChecked]: isChecked && disabled,
+    }
 
     return (
-      <div className={cls.container}>
+      <div tabIndex={0} className={cls.container}>
         <div
-          tabIndex={0}
           onClick={onClickHandler}
           style={{ width: width, height: height }}
-          className={classNames(cls.CheckBox, {[cls.isDisabled]: disabled, [cls.isChecked]: value}, [className])}
+          className={classNames(cls.CheckBox, mods, [className])}
         >
-          <div className={classNames(cls.decoration, {}, [])}></div>
-          {value && !disabled && <Image src={check} alt={'check'} width={14} height={14} />}
-          {disabled && value && <Image src={disabledCheckbox} alt={'disabled'} width={14} height={14} />}
+          <div className={cls.decoration}></div>
+          {isChecked && !disabled && <Image src={check} alt={'check'} width={14} height={14} />}
+          {disabled && isChecked && (
+            <Image src={disabledCheckbox} alt={'disabled'} width={14} height={14} />
+          )}
         </div>
         <label className={cls.label}>
           {label}
 
           <input
-            checked={value}
+            checked={isChecked}
             onChange={onChange}
             className={cls.input}
             id={'check'}
