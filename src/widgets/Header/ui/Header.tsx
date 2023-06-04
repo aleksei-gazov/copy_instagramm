74e47dbe5ed0d121505d22ced0a52c00c50d0415
Sergey Saprankov <sergey-saprankov@mail.ru>
2023-06-04 20:24:57 +0300

    refactor: delete logout with header

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,21 +1,13 @@
-import { useRouter } from 'next/router'
-
 import cls from './Header.module.scss'
 
-import { LogOutComponent } from 'features/auth/logOut/ui/LogOutComponent'
-import { PATH } from 'shared/const/path'
 import { Text, TextColorTheme, TextFontTheme } from 'shared/ui/Text/Text'
 
 export const Header = () => {
-  const { asPath } = useRouter()
-  const hide = asPath === PATH.PROFILE_SETTING
-
   return (
     <header className={cls.Header}>
       <Text tag={'span'} color={TextColorTheme.LIGHT} font={TextFontTheme.INTER_SEMI_BOLD_XL}>
         Inctagram
       </Text>
-      {hide && <LogOutComponent />}
     </header>
   )
 }
