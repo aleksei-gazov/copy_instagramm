085fe274e0402595b6000b49213263fd30dd6781
MAKS <5059585@mail.ru>
2023-06-06 13:36:56 +0300

    fix: UserSetting Photo

--- src/features/profile/profileSetting/ui/profilePhoto/profilePhotoBlock.tsx
+++ src/features/profile/profileSetting/ui/profilePhoto/profilePhotoBlock.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Photo from '../../../../../../public/icon/photo.svg'
+
 import cls from './profilePhotoBlock.module.scss'
 
 import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button'
@@ -7,7 +9,9 @@ import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button'
 export const ProfilePhotoBlock = () => {
   return (
     <div className={cls.photoBlock}>
-      <div className={cls.photo}>Photo</div>
+      <div className={cls.photo}>
+        <Photo />
+      </div>
       <Button theme={ButtonTheme.OUTLINE} className={cls.addButton} size={ButtonSize.M}>
         Add a Profile Photo
       </Button>
