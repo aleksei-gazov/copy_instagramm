af4e7478f6b849f8801466c1f818543fc8544557
grigorydobrenko <grigorydobrenko97@gmail.com>
2023-06-08 15:04:05 +0300

    feat: add centering and cropping avatar

--- src/features/profile/profileSetting/photoSetting/ui/SettingPhotoModal/SettingPhotoModal.tsx
+++ src/features/profile/profileSetting/photoSetting/ui/SettingPhotoModal/SettingPhotoModal.tsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
+
+import AvatarEditor from 'react-avatar-editor'
 
 import Photo from '../../../../../../../public/icon/photo.svg'
 
 import cls from './SettingPhotoModal.module.scss'
 
-import { SelectedImage } from 'features/profile/profileSetting/photoSetting/ui/SelectedImage/SelectedImage'
+import { classNames } from 'shared/lib/classNames/classNames'
+import { Button, ButtonTheme } from 'shared/ui/Button/Button'
 import { InputTypeFile } from 'shared/ui/InputTypeFile/InputTypeFile'
 import { ModalHeader } from 'shared/ui/ModalHeader/ModalHeader'
 import { Portal } from 'shared/ui/Portal/Portal'
@@ -16,6 +19,25 @@ export type SettingPhotoModalType = {
 
 export const SettingPhotoModal = ({ isModalOpen, setIsModalOpen }: SettingPhotoModalType) => {
   const [selectedImage, setSelectedImage] = useState<File | null>(null)
+  const editorRef = useRef<AvatarEditor>(null)
+  const [position, setPosition] = useState({ x: 0.5, y: 0.5 })
+
+  const handleSaveAvatar = () => {
+    if (editorRef.current) {
+      const canvas = editorRef.current.getImageScaledToCanvas()
+      const dataUrl = canvas.toDataURL('image/jpeg')
+
+      setIsModalOpen(false)
+      setSelectedImage(null)
+      console.log(dataUrl)
+      // Perform necessary actions with the centered avatar dataUrl
+      // (e.g., upload to the server)
+    }
+  }
+
+  const handlePositionChange = (position: { x: number; y: number }) => {
+    setPosition(position)
+  }
   const handleButtonClick = () => {
     setIsModalOpen(false)
     setSelectedImage(null)
@@ -28,15 +50,51 @@ export const SettingPhotoModal = ({ isModalOpen, setIsModalOpen }: SettingPhotoM
       <div className={cls.container}>
         <ModalHeader title={'Add a Profile Photo'} handleButtonClick={handleButtonClick} />
         <div className={cls.main}>
-          <div className={cls.photoContainer}>
+          <div
+            className={classNames(
+              cls.photoContainer,
+              {
+                [cls.emptyPhotoContainer]: selectedImage === null,
+              },
+              []
+            )}
+          >
             {selectedImage ? (
-              <SelectedImage selectedImage={selectedImage} />
+              <AvatarEditor
+                ref={editorRef}
+                image={selectedImage}
+                width={316}
+                height={316}
+                color={[23, 23, 23, 0.6]}
+                backgroundColor={'black'}
+                scale={1}
+                borderRadius={155}
+                position={position}
+                onPositionChange={handlePositionChange}
+                crossOrigin="anonymous"
+                disableBoundaryChecks={false}
+              />
             ) : (
               <Photo className={cls.photo} />
             )}
           </div>
-          <div className={cls.selectPhoto}>
-            <InputTypeFile setSelectedImage={setSelectedImage} />
+          <div
+            className={classNames(cls.btnContainer, {
+              [cls.selectPhoto]: selectedImage === null,
+              [cls.save]: selectedImage !== null,
+            })}
+          >
+            {selectedImage ? (
+              <Button
+                theme={ButtonTheme.PRIMARY}
+                className={cls.saveBtn}
+                onClick={handleSaveAvatar}
+              >
+                Save
+              </Button>
+            ) : (
+              <InputTypeFile setSelectedImage={setSelectedImage} />
+            )}
           </div>
         </div>
       </div>
