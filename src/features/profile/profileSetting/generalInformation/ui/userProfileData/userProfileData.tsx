cce47033eecd2e0b47df264d3caa0b3bb8de6f72
zemosamurai <zemosamurai@gmail.com>
2023-06-09 17:34:59 +0300

    ref: add control input

--- src/features/profile/profileSetting/generalInformation/ui/userProfileData/userProfileData.tsx
+++ src/features/profile/profileSetting/generalInformation/ui/userProfileData/userProfileData.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
-
-import { Controller, FieldValues } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { Controller, useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import * as Yup from 'yup'
 
 import {
   useDelProfileMutation,
@@ -12,102 +13,89 @@ import cls from './userProfileData.module.scss'
 
 import { getAuthMeData } from 'shared/hoc'
 import { useAppSelector } from 'shared/hooks/useAppSelector'
-import { useFormHandler } from 'shared/hooks/useFormHandler'
-import { useSetValuesFromProfileData } from 'shared/hooks/useSetValuesFromProfileData'
 import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button'
+import { ControlledInputNew } from 'shared/ui/ControlledInput/ControlledInput'
 import { CustomDatePicker } from 'shared/ui/DatePicker/DatePicker'
 import { Input } from 'shared/ui/Input/Input'
 import { LoaderContent } from 'shared/ui/LoaderContent/LoaderContent'
 import { TextArea } from 'shared/ui/TextArea/TextArea'
 
+// ----------------------------------
+
+const userProfileDataFormSchema = yup.object().shape({
+  userName: Yup.string().required().min(6).max(30),
+  firstName: Yup.string().required(),
+  lastName: Yup.string().required(),
+  dateOfBirth: Yup.date().required(),
+  city: Yup.string().required(),
+  aboutMe: Yup.string().required(),
+})
+
+type FormData = yup.InferType<typeof userProfileDataFormSchema>
+const useProfileDataForm = () => {
+  return useForm<FormData>({
+    mode: 'onSubmit',
+    resolver: yupResolver(userProfileDataFormSchema),
+  })
+}
+
+// -------------------------------------
+
 export const UserProfileData = () => {
-  const {
-    isValid,
-    register,
-    handleSubmit,
-    errorName,
-    errorCity,
-    errorFirstName,
-    errorLastName,
-    control,
-    setValue,
-  } = useFormHandler('name', 'firstName', 'lastName', 'city', 'textArea')
+  const { control, handleSubmit, setValue } = useProfileDataForm()
   const authMeData = useAppSelector(getAuthMeData)
   const userId = authMeData?.userId
-  const { data: profileData, isLoading: isLoadingGetProfile } = useGetProfileQuery(userId)
+  const {
+    data: profileData,
+    isLoading: isLoadingGetProfile,
+    isSuccess,
+  } = useGetProfileQuery(userId)
   const [profile, { isLoading: isLoadingUpdateProfile }] = useUpdateProfileMutation()
   const [delProfile] = useDelProfileMutation()
 
-  useSetValuesFromProfileData(setValue, profileData)
+  const onSubmit = handleSubmit(data => {
+    profile(data)
+  })
 
-  const onSubmit = (data: FieldValues) => {
-    const payload = {
-      userName: data.name,
-      firstName: data.firstName,
-      lastName: data.lastName,
-      city: data.city,
-      dateOfBirth: data.date,
-      aboutMe: data.textArea,
-    }
-
-    profile(payload)
+  if (isSuccess) {
+    setValue('userName', profileData.userName)
+    setValue('firstName', profileData.firstName)
+    setValue('lastName', profileData.lastName)
+    setValue('dateOfBirth', profileData.dateOfBirth)
+    setValue('city', profileData.city)
+    setValue('aboutMe', profileData.aboutMe)
   }
 
   return (
-    <form className={cls.form} onSubmit={handleSubmit(onSubmit)}>
+    <form className={cls.form} onSubmit={onSubmit}>
       <div className={cls.form}>
         {(isLoadingGetProfile || isLoadingUpdateProfile) && <LoaderContent />}
-        <Input
-          autoFocus
-          register={register}
-          nameForValidate={'name'}
-          error={errorName}
-          defaultValue={profileData?.userName}
-          title={'User Name'}
-        />
-        <Input
-          register={register}
-          nameForValidate={'firstName'}
-          error={errorFirstName}
-          defaultValue={profileData?.firstName}
-          title={'First Name'}
-        />
-        <Input
-          register={register}
-          nameForValidate={'lastName'}
-          error={errorLastName}
-          defaultValue={profileData?.lastName}
-          title={'Last Name'}
-        />
-        <Controller
-          control={control}
-          name="date"
-          defaultValue={profileData?.dateOfBirth}
-          render={({ field }) => (
-            <CustomDatePicker
-              title={'Date of birthday'}
-              start={field.value}
-              onChange={date => field.onChange(date)}
-            />
-          )}
-        />
-        <Input
-          defaultValue={profileData?.city}
-          register={register}
-          nameForValidate={'city'}
-          error={errorCity}
-          title={'City'}
-        />
-        <TextArea
-          defaultValue={profileData?.aboutMe}
-          register={register}
-          nameForValidate={'textArea'}
-          title={'About Me'}
-        />
+        <ControlledInputNew control={control} name={'userName'} title={'User Name'} />
+        <ControlledInputNew control={control} name={'firstName'} title={'First Name'} />
+        <ControlledInputNew control={control} name={'lastName'} title={'Last Name'} />
+        {/*<Controller*/}
+        {/*  control={control}*/}
+        {/*  name="date"*/}
+        {/*  defaultValue={profileData?.dateOfBirth}*/}
+        {/*  render={({ field }) => (*/}
+        {/*    <CustomDatePicker*/}
+        {/*      title={'Date of birthday'}*/}
+        {/*      start={field.value}*/}
+        {/*      onChange={date => field.onChange(date)}*/}
+        {/*    />*/}
+        {/*  )}*/}
+        {/*/>*/}
+        <ControlledInputNew control={control} name={'city'} title={'City'} />
+        {/*<TextArea*/}
+        {/*  defaultValue={profileData?.aboutMe}*/}
+        {/*  register={register}*/}
+        {/*  nameForValidate={'textArea'}*/}
+        {/*  title={'About Me'}*/}
+        {/*/>*/}
       </div>
       <div className={cls.decor}></div>
       <Button
-        disabled={!isValid}
+        // disabled={!isValid}
         type={'submit'}
         theme={ButtonTheme.PRIMARY}
         size={ButtonSize.XS}
@@ -117,7 +105,7 @@ export const UserProfileData = () => {
       </Button>
       {/*//TODO*/}
       {/*delete button*/}
-      <button onClick={e => delProfile(userId)}>DEL PROFILE</button>
+      {/*<button onClick={e => delProfile(userId)}>DEL PROFILE</button>*/}
     </form>
   )
 }
