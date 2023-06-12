4f6168361f532db6787624cc68b02e5fc03d1b0f
Sergey Saprankov <sergey-saprankov@mail.ru>
2023-06-12 16:13:44 +0300

    feat: user profile features and fake data for test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,6 @@
-import React from 'react'
-
-import { useRouter } from 'next/router'
-
 import { getLayout } from 'widgets/Layout/Layout'
 
 function Home() {
-  const { push } = useRouter()
-
   return <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>Home</div>
 }
 
