9b91566af9dd2608992854dc37c87b83615bbb43
grigorydobrenko <grigorydobrenko97@gmail.com>
2023-06-17 15:18:45 +0300

    feat: implement tracking a ref element

--- src/features/profile/userProfile/ui/UserProfileContent/UserProfileContent.tsx
+++ src/features/profile/userProfile/ui/UserProfileContent/UserProfileContent.tsx
@@ -1,4 +1,6 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+
+import { useInView } from 'react-intersection-observer'
 
 import cls from './UserProfileContent.module.scss'
 
@@ -11,31 +13,49 @@ import { Loader } from 'shared/ui/Loader/Loader'
 
 export const UserProfileContent = () => {
   const [currentId, setCurrentId] = useState<null | number>(null)
+  const [page, setPage] = useState<number>(1)
+
   const userId = useAppSelector(getUserId)
 
   const { data, isLoading } = useGetPostsQuery(userId, {
     skip: !userId,
   })
 
+  const { ref, inView } = useInView()
+
   const getCurrentPostId = useCallback((id: number | null) => {
     setCurrentId(id)
   }, [])
 
+  useEffect(() => {
+    // console.log(page)
+    // useGetPostsQuery({ userId, page });
+  }, [page])
+
+  useEffect(() => {
+    if (inView) {
+      setPage(prevPage => prevPage + 1)
+    }
+  }, [inView])
+
   if (isLoading) return <Loader />
 
   return (
-    <div className={cls.UserProfileContent}>
-      {data &&
-        data?.items.map(el => (
-          <Card
-            id={el.id}
-            key={el.id}
-            callBack={getCurrentPostId}
-            src={el.images[0].url}
-            alt={el.description}
-          />
-        ))}
-      {currentId && <Post callBack={getCurrentPostId} currentId={currentId} />}
-    </div>
+    <>
+      <div className={cls.UserProfileContent}>
+        {data &&
+          data?.items.map(el => (
+            <Card
+              id={el.id}
+              key={el.id}
+              callBack={getCurrentPostId}
+              src={el.images[0].url}
+              alt={el.description}
+            />
+          ))}
+        {currentId && <Post callBack={getCurrentPostId} currentId={currentId} />}
+      </div>
+      <div ref={ref}></div>
+    </>
   )
 }
